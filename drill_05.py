a3b8516b1472fe9bc8c8fa4c7e21be01e51978fb
dasira <50608526+dasira@users.noreply.github.com>
2019-10-07 19:14:28 +0900

    구현
    마우스 움직임
    캐릭터 클릭한 방향 으로 순간이동
    캐릭터 피봇이 검지에 닿게 이동함

--- drill_05.py
+++ drill_05.py
@@ -5,8 +5,8 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def handle_events():
     global running
-    global x,y
-    global charX, charY
+    global x, y
+    global P1, P2
 
     events = get_events()
     for event in events:
@@ -15,22 +15,20 @@ def handle_events():
         elif event.type == SDL_MOUSEMOTION:
             x,y = event.x +25, KPU_HEIGHT - event.y - 26
         elif event.type == SDL_MOUSEBUTTONDOWN:
-            charX, charY = event.x, KPU_HEIGHT - event.y
+            P2= [event.x, KPU_HEIGHT - event.y]
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
 
-def draw_line(p1, p2):
-    draw_big_point(p1)
-    draw_big_point(p2)
-
+def move_char(p1, p2):
     for i in range(0,100 + 1, 2):
         t = i / 100
         x = (1-t) * p1[0] + t*p2[0]
         y = (1-t) * p1[1] + t*p2[1]
-        draw_point((x,y))
-
-    draw_point(p2)
+        if P1[0] <= P2[0]:
+            character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+        else:
+            character.clip_draw(frame * 100, 0, 100, 100, x, y)
     pass
 
 
@@ -38,18 +36,21 @@ open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 hand_arrow = load_image('hand_arrow.png')
-charX=640
-charY=512
+
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+P1 = [640,512]
+P2 = [640,512]
 frame = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, charX, charY)
+
+    move_char(P1,P2)
+
     hand_arrow.draw(x, y)
     update_canvas()
     frame = (frame + 1) % 8
